eefda998a7a008d42d7c958c72fd23455e2d2ed4
rflinxx <raflinapin5@gmail.com>
2026-07-24 13:28:20 +0700

    Update Sanctum

--- routes/api.php
+++ routes/api.php
@@ -2,15 +2,33 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Hash;
+use App\Models\User;
 
 use App\Http\Controllers\Api\CustomerApiController;
 use App\Http\Controllers\Api\VehicleApiController;
 use App\Http\Controllers\Api\TransactionApiController;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
 
-Route::get('/customers', [CustomerApiController::class, 'index']);
-Route::get('/vehicles', [VehicleApiController::class, 'index']);
-Route::get('/transactions', [TransactionApiController::class, 'index']);
+Route::post('/login', function (Request $request) {
+    $user = User::where('email', $request->email)->first();
+
+    if (!$user || !Hash::check($request->password, $user->password)) {
+        return response()->json(['message' => 'Email atau password salah'], 401);
+    }
+
+    return response()->json([
+        'token' => $user->createToken('auth_token')->plainTextToken
+    ]);
+});
+
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('/user', function (Request $request) {
+        return $request->user();
+    });
+
+    Route::get('/customers', [CustomerApiController::class, 'index']);
+    Route::get('/vehicles', [VehicleApiController::class, 'index']);
+    Route::get('/transactions', [TransactionApiController::class, 'index']);
+});
